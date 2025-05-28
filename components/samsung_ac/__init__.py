39ad00933a5a228525bfc313c2191725db5eca3e
mnvr1991 <83885255+mnvr1991@users.noreply.github.com>
2025-05-28 15:25:04 +0200

    Update __init__.py
    Replace UNDEF to UNDEFINED

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -143,12 +143,12 @@ CUSTOM_SENSOR_SCHEMA = sensor.sensor_schema().extend(
 
 def custom_sensor_schema(
     message: int,
-    unit_of_measurement: str = sensor._UNDEF,
-    icon: str = sensor._UNDEF,
-    accuracy_decimals: int = sensor._UNDEF,
-    device_class: str = sensor._UNDEF,
-    state_class: str = sensor._UNDEF,
-    entity_category: str = sensor._UNDEF,
+    unit_of_measurement: str = sensor._UNDEFINED,
+    icon: str = sensor._UNDEFINED,
+    accuracy_decimals: int = sensor._UNDEFINED,
+    device_class: str = sensor._UNDEFINED,
+    state_class: str = sensor._UNDEFINED,
+    entity_category: str = sensor._UNDEFINED,
     raw_filters=[],
 ):
     return sensor.sensor_schema(
